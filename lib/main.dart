90518bba8656b9c425bf26a470a7e5f45d4e53c8
Bhaven Kakade <thisisbhaven@gmail.com>
2020-04-11 21:59:57 +0530

    fin with random numbers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,21 +16,53 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int lDice = 1;
+  int rDice = 1;
+
+  void changeDice() {
+    lDice = Random().nextInt(6) + 1;
+    rDice = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Center(
+      child: Row(
         children: <Widget>[
           Expanded(
-            child: Image.asset('images/dice1.png'),
-          ),
-          SizedBox(
-            width: 20.0,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    changeDice();
+                  });
+                },
+                child: Image.asset('images/dice$lDice.png'),
+              ),
+            ),
           ),
           Expanded(
-            child: Image.asset('images/dice2.png'),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    changeDice();
+                  });
+                },
+                child: Image.asset('images/dice$rDice.png'),
+              ),
+            ),
           ),
         ],
-      );
+      ),
+    );
   }
 }
