be18902e63ab06eb6ef60bf4e36e7f9da7a36331
Bhaven Kakade <thisisbhaven@gmail.com>
2020-04-11 18:52:41 +0530

    add two dices

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,18 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+        children: <Widget>[
+          Expanded(
+            child: Image.asset('images/dice1.png'),
+          ),
+          SizedBox(
+            width: 20.0,
+          ),
+          Expanded(
+            child: Image.asset('images/dice2.png'),
+          ),
+        ],
+      );
   }
 }
